f4163bfd6624e85d3d875fffe0cec7517087fa54
Luca Di Gaspero <luca.digaspero@uniud.it>
2025-06-17 17:05:29 +0200

    Amended solution.

--- problems/autocarrier-loading/solution/solution.py
+++ problems/autocarrier-loading/solution/solution.py
@@ -5,12 +5,10 @@ class ACLSolution():
 
     def from_json(self, json_data):
         """Load the solution from a JSON string."""
-        import json
-        data = json.loads(json_data)
-        for assignment in data:
+        for assignment in json_data:
             vehicle_id = assignment['vehicle']
             deck_id = assignment['deck']
-            self.deck_assignment[vehicle] = deck
+            self.deck_assignment[vehicle_id] = deck_id
 
     def __repr__(self):
         """Return a string representation of the solution."""
